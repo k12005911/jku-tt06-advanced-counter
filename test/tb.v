ca8557058c510d473878a70b5a2f39861c6279a9
k12005911 <k12005911@students.jku.at>
2024-02-12 17:28:59 +0100

    fix tb.v?

--- test/tb.v
+++ test/tb.v
@@ -23,7 +23,7 @@ module tb ();
   wire [7:0] uio_oe;
 
   // Replace tt_um_example with your module name:
-  tt_um_advanced_counter (
+  tt_um_advanced_counter #(2)(
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
