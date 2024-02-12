b14ef5c7e3da311932dafc874556fbe316b19655
k12005911 <k12005911@students.jku.at>
2024-02-12 15:07:18 +0100

    Typo in module name

--- src/tt_um_advanced_counter.v
+++ src/tt_um_advanced_counter.v
@@ -7,7 +7,7 @@
 `include "insynch.v"
 */
 
-module tt_um_spellcounter #(parameter DIGITS = 2)(
+module tt_um_advanced_counter #(parameter DIGITS = 2)(
 
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
