25a2bcebc6f3bc452be26d656d80391d73514be1
k12005911 <k12005911@students.jku.at>
2024-02-21 19:14:11 +0100

    testing changes

--- src/input_trigger.v
+++ src/input_trigger.v
@@ -56,7 +56,7 @@ module input_trigger #( parameter DIGITS = 6) (
         	case(State)
         	//No Reaction for 10240 clock cycles (= 10ms), debouncing of inputs
     		DebounceBlock: begin
-	    		if (counter == 'd0) begin
+	    		if (counter == 'd16380) begin
 				State <= Ready;
 			end
 			counter <= counter + 'd1;
@@ -70,16 +70,16 @@ module input_trigger #( parameter DIGITS = 6) (
     			//if new signal is active, start output of pulses
     			if ((trigger & ~active_triggers) != 'd0) begin
     				State <= Calculation;
-		    		counter <= 'd0;
+		    		counter <= 'd16380;
 		    		inc_flag <= 1'b1;
 		    		ref_flag <= 1'b0;
     			end 
     		end
     		//Wait for 16 cycles for the counters to finish (in case of carry over)
     		Calculation: begin
-    			if (counter >= 'd16) begin
+    			if (counter >= 'd16389) begin
 				State <= Refresh;
-	    			counter <= 'd16;
+	    			counter <= 'd16389;
 				ref_flag <= 1'b1;
 			end else begin
 	    			counter <= counter + 'd1;
